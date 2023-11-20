6277cf5a0cc30a72dd78cd7d219c7e5ea1991756
nklvnsta <nastanikol10105@gmail.com>
2023-11-20 15:38:26 +0300

    Roleseeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -22,7 +18,6 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -32,7 +27,6 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
     /**
      * The attributes that should be cast.
      *
@@ -40,6 +34,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+
+    public function role(){
+        $this->belongsTo(Role::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Database\Seeders\ArticleSeeder;
+use Database\Seeders\RoleSeeder;
 use App\Models\Article;
 use App\Models\Comment;
 
@@ -15,14 +17,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // Article::factory(10)->create();
-        $this->call([
-            ArticleSeeder::class,
-        ]);
         Article::factory(10)->has(Comment::factory(3))->create();
         // $this->call([
         //     ArticleSeeder::class,
         // ]);
+        $this->call([
+            ArticleSeeder::class,
+            RoleSeeder::class,
+        ]);
 
     }
 }
